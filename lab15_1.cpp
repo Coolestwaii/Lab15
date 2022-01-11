b2f464fe6d0ee08cf82feb470e5ca8019fb1240e
waii <yougle55567@gmail.com>
2022-01-11 20:45:54 +0700

    lab15 done

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	int b;
+	T key;
+	for(int a = 1; a < N; a++){
+		key = d[a];
+		b = a - 1;
+		while(b >= 0 && key > d[b]){
+			d[b + 1] = d[b];
+			b = b - 1;
+		}
+		d[b + 1] = key;
+		
+		cout << "Pass " << a << ":";
+		for(int x = 0; x < N; x++)
+		cout << d[x] << " ";
+		cout << "\n";
+	}
 }
 
 int main(){
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,38 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double a[][N]){
+	for(int i = 0; i < N; i++){
+		cout << "Row " << i + 1 << ": ";
+		for(int j = 0; j < N; j++){
+			cin >> a[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double b[][N], bool c[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			c[i][j] = false;
+		}
+	}
+	for(int i = 0; i < N - 2; i++){
+		for(int j = 0; j < N - 2; j++){
+			if(b[i + 1][j + 1] >= b[i + 2][j + 1] && b[i + 1][j + 1] >= b[i + 1][j] && b[i + 1][j + 1] >= b[i + 1][j + 2] && b[i + 1][j + 1] >= b[i][j + 1]){
+				c[i + 1][j + 1] = true;
+			}else{
+				c[i + 1][j + 1] = false;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool d[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			cout << d[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
